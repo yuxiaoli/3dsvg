022590c0874625c885308b3471baa2d3f3667668
Sean Li <sean.li@vectorindex.cloud>
2026-05-25 12:50:51 +0800

    fix: next.config.ts basePath for github pages

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = "";
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+}
+
 const nextConfig: NextConfig = {
   transpilePackages: ["3dsvg"],
   output: "export",
+  basePath: isGithubActions ? `/${repo}` : "",
+  assetPrefix: isGithubActions ? `/${repo}/` : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
